286842eb55a05efb5e0a0f3718651c17ba99cba5
kjyeon1101 <kjyeon1101@gmail.com>
2023-11-19 15:35:51 +0900

    #1 feat: secret & db setting

--- src/main/java/sogang/capstoneDesign2/newSnack/NewSnackApplication.java
+++ src/main/java/sogang/capstoneDesign2/newSnack/NewSnackApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewSnackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NewSnackApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(NewSnackApplication.class, args);
+  }
 
 }
