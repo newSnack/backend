86c624b21560f181393cf24e40a493aca32b7602
kjyeon1101 <kjyeon1101@gmail.com>
2023-11-19 14:45:15 +0900

    #1 feat: initial project setting

---
+++ src/main/java/sogang/capstoneDesign2/newSnack/NewSnackApplication.java
@@ -0,0 +1,13 @@
+package sogang.capstoneDesign2.newSnack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSnackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSnackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sogang/capstoneDesign2/newSnack/NewSnackApplicationTests.java
@@ -0,0 +1,13 @@
+package sogang.capstoneDesign2.newSnack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSnackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
